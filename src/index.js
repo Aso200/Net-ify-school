535221e18ee5241596b7080e8d21a05379e915d7
Aso200 <asmir_ramiz@hotmail.com>
2023-06-18 19:46:59 +0200

    klart

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const calculatorResult = require('@aso200/calculator-result');
-
+const calculatorResult = require("@aso200/calculator-result");
+const resultElement = document.getElementById("result");
 
 const result = calculatorResult(2, 3, 4);
 console.log(result);
+
+resultElement.textContent = `Resultat: ${result}`;
