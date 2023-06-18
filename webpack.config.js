b8d1feef055bb2ca798c4f12fef28a065245266f
Aso200 <asmir_ramiz@hotmail.com>
2023-06-18 20:10:31 +0200

    ny sak

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,5 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'), // The output directory for bundled files
     filename: 'bundle.js' // The name of the output bundle file
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // Apply the following rules to JavaScript files
-        exclude: /node_modules/, // Exclude the node_modules directory
-        use: 'babel-loader' // Use the babel-loader for transpiling JavaScript files
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js'] // List of file extensions to resolve
   }
-};
+}
